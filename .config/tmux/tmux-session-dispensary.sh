17a24554828a1537c67f9d9c080aec60ad025510
Johnny Urosevic <themagicalcake@gmail.com>
2025-10-22 23:43:14 -0700

    When I spawn a tmux session open nvim and lazygit

--- .config/tmux/tmux-session-dispensary.sh
+++ .config/tmux/tmux-session-dispensary.sh
@@ -21,6 +21,14 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 if ! tmux has-session -t "$selected_name"; then
     tmux new-session -ds "$selected_name" -c "$selected"
+
+    tmux new-window -t "$selected_name:2" -c "$selected" nvim .
+    if [ -d "$selected/.git" ]; then
+        tmux new-window -t "$selected_name:3" -c "$selected" lazygit
+    elif [[ $selected == *".config"* ]]; then
+        tmux new-window -t "$selected_name:3" lazygit -ucd ~/.local/share/yadm/lazygit -w ~ -g ~/.local/share/yadm/repo.git
+    fi
+
     tmux select-window -t "$selected_name:1"
 fi
 
