9e0e07d4ffd058fcf0b4085eab93337b9cd38fb4
Johnny Urosevic <themagicalcake@gmail.com>
2026-05-31 00:50:10 -0700

    Swapped my monitor inputs

--- .config/autostart/autostart.sh
+++ .config/autostart/autostart.sh
@@ -4,6 +4,10 @@ picom &
 xset s -dpms
 xset s off
 
+xinput set-prop 15 "libinput Accel Profile Enabled Default" 0, 1, 0
+
+wlr-randr --output DP-2 --mode 1920x1080@143.996002 --adaptive-sync disabled --right-of DP-1 --output DP-1 --mode 2560x1440@165 --adaptive-sync disabled --left-of DP-2
+
 qtile cmd-obj -o cmd -f reload_config
 
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
